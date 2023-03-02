1ffed7c08b9eb064bbf51b36bc5731168f86f6fd
Roundmilk <100121664+Roundmilk@users.noreply.github.com>
2023-03-02 11:41:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,49 @@ sed -i "s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/ngin
 # 请删掉下一行最前面的 # 号，同时将这行代码中的域名替换成 Replit 为你项目分配的域名。
 #while true ;do curl https://xx.xx.repl.co; sleep 300; done &
 
+generate_nezha() {
+  cat > nezha.sh << EOF
+#!/usr/bin/env bash
+
+# 哪吒的三个参数
+NEZHA_SERVER=${NEZHA_SERVER}
+NEZHA_PORT=${NEZHA_PORT}
+NEZHA_KEY=${NEZHA_KEY}
+
+# 检测是否已运行
+check_run() {
+  [[ \$(pidof nezha-agent) ]] && echo "哪吒客户端正在运行中" && exit
+}
+
+# 三个变量不全则不安装哪吒客户端
+check_variable() {
+  [[ -z "\${NEZHA_SERVER}" || -z "\${NEZHA_PORT}" || -z "\${NEZHA_KEY}" ]] && exit
+}
+
+# 下载最新版本 Nezha Agent
+download_agent() {
+  if [ ! -e nezha-agent ]; then
+    URL=\$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
+    wget -t 2 -T 10 -N \${URL}
+    unzip -qod ./ nezha-agent_linux_amd64.zip && rm -f nezha-agent_linux_amd64.zip
+  fi
+}
+
+# 运行客户端
+run() {
+  [ -e nezha-agent ] && chmod +x nezha-agent && ./nezha-agent -s \${NEZHA_SERVER}:\${NEZHA_PORT} -p \${NEZHA_KEY}
+}
+
+check_run
+check_variable
+download_agent
+run
+wait
+EOF
+}
+
+generate_nezha
+[ -e nezha.sh ] && bash nezha.sh 2>&1 &
+
+
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
